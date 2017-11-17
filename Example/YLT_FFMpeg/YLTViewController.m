ceec72b403206a317d418b45882b14b3e865f7c1
Alex <xphaijj0305@126.com>
2017-11-17 21:06:20 +0800

    release 0.0.1

---
+++ Example/YLT_FFMpeg/YLTViewController.m
@@ -0,0 +1,30 @@
+//
+//  YLTViewController.m
+//  YLT_FFMpeg
+//
+//  Created by xphaijj on 11/16/2017.
+//  Copyright (c) 2017 xphaijj. All rights reserved.
+//
+
+#import "YLTViewController.h"
+
+@interface YLTViewController ()
+
+@end
+
+@implementation YLTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
